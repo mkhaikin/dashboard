6dd3c26c9d40d16dbee478eec2d85b29e00fc4b8
mkhaikin <michael.khaikin@gmail.com>
2019-12-27 11:38:26 -0500

    admin and dashboard routes

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
+const routes = require('./routes');
+
 // recognize incoming req as JSON Object
 app.use(express.json());
 // Handlebars
@@ -12,7 +14,7 @@ app.set('view engine', 'handlebars');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 // initial express routes
-//app.use('/', routes());
+app.use('/', routes());
 // Set a static folder in the public directory
 app.use(express.static('public'));
 // silence the server request for a fac icon
@@ -26,7 +28,7 @@ app.get('/', (req,res,next) => {
 
 // Active on port #
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on http://localhost:${PORT}`));
 
 //Export module to be used by others
 module.export = app;

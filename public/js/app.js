bc8b275230a4bbd8c7809a506788c7b90cec36d4
mkhaikin <michael.khaikin@gmail.com>
2020-02-10 22:27:05 -0500

    added full screen btn

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,29 @@
-/**/
+/* Toggle full-screen*/
+var elem = document.documentElement;
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) { /* Firefox */
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE/Edge */
+    elem.msRequestFullscreen();
+  }
+}
+
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullScreen) {
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) {
+    document.msExitFullscreen();
+  }
+}
+
 const noticeTemplate = (id, text, start, end, srcImg) => {
 
     const noticeBox = $('<div>').attr({
